27f1105cd5fa912d40a2acc20b9e6729c5679c1a
Jon Beaulieu <jon@jonbeaulieu.com>
2021-04-09 15:32:00 -0500

    Date check bugfix, scrolling, notes length limit
    Created a new helper function to check if there are unsaved task changes. Fixed a case where an empty due date would cause a null exception. Added a scrollview to the fragment, and limited notes to 500 characters.

--- app/src/main/java/com/jbproductions/liszt/DetailsFragment.java
+++ app/src/main/java/com/jbproductions/liszt/DetailsFragment.java
@@ -3,7 +3,8 @@ package com.jbproductions.liszt;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,16 +14,16 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-
 import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -36,7 +37,8 @@ public class DetailsFragment extends Fragment {
     private CheckBox taskStatusCheckBox;
     private TextView dueDateTextView;
     private ImageButton removeDueDateButton;
-    private EditText taskNotesEditText;
+    private TextInputLayout taskNotesLayout;
+    private TextInputEditText taskNotesEditText;
     private ViewModel viewModel;
 
     private Task referenceTask;
@@ -49,25 +51,18 @@ public class DetailsFragment extends Fragment {
         OnBackPressedCallback checkUnsavedChangesCallback = new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
-
-                // Check if there are unsaved changes
-                if (taskNameEditText.getText().toString().equals(referenceTask.getName())
-                        && taskStatusCheckBox.isChecked() == referenceTask.getComplete()
-                        && viewModel.selectedTask.getDueDate() == null ? referenceTask.getDueDate() == null
-                            : viewModel.selectedTask.getDueDate().equals(referenceTask.getDueDate())
-                        && taskNotesEditText.getText().toString().equals(referenceTask.getNotes())) {
-                    // No changes have been made, go back
-                    NavHostFragment.findNavController(DetailsFragment.this).popBackStack();
-                } else {
+                if (hasUnsavedChanges()) {
                     // There are unsaved changes, prompt the user before discarding
                     AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity())
-                            .setTitle("Discard Changes?")
-                            .setPositiveButton("YES", (dialogInterface, i) -> {
+                            .setTitle(R.string.discard_changes_confirmation)
+                            .setPositiveButton(R.string.yes, (dialogInterface, i) -> {
                                 NavHostFragment.findNavController(DetailsFragment.this).popBackStack();
                             })
-                            .setNegativeButton("NO", (dialogInterface, i) -> {});
-
+                            .setNegativeButton(R.string.no, (dialogInterface, i) -> {});
                     builder.show();
+                } else {
+                    // No changes have been made, go back
+                    NavHostFragment.findNavController(DetailsFragment.this).popBackStack();
                 }
             }
         };
@@ -82,16 +77,13 @@ public class DetailsFragment extends Fragment {
         taskNameEditText = view.findViewById(R.id.task_name_text);
         taskStatusCheckBox = view.findViewById(R.id.task_checkbox);
         dueDateTextView = view.findViewById(R.id.due_date_text);
+        taskNotesLayout = view.findViewById(R.id.task_notes_layout);
         taskNotesEditText = view.findViewById(R.id.task_notes_text);
         removeDueDateButton = view.findViewById(R.id.remove_due_date_button);
 
         // Set the ActionBar title and show the up/back button
-        ((AppCompatActivity) requireActivity()).getSupportActionBar().setTitle("Edit");
+        ((AppCompatActivity) requireActivity()).getSupportActionBar().setTitle(R.string.details_fragment_title);
         ((AppCompatActivity) requireActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        // Set click listener on due date field to open the DatePickerDialog
-        CardView dueDatePicker = view.findViewById(R.id.datePickerCard);
-        dueDatePicker.setOnClickListener(v -> createDatePicker());
 
         //Get a reference to the app's ViewModel, so that we can access data for the selected task
         viewModel = new ViewModelProvider(requireActivity()).get(ViewModel.class);
@@ -112,12 +104,6 @@ public class DetailsFragment extends Fragment {
         }
         taskNotesEditText.setText(viewModel.selectedTask.getNotes());
 
-        removeDueDateButton.setOnClickListener(v -> {
-            viewModel.selectedTask.setDueDate(null);
-            dueDateTextView.setText("Set Due Date");
-            removeDueDateButton.setVisibility(View.GONE);
-        });
-
         // Set listeners on the name field to maintain focus and show/hide the cursor & keyboard appropriately
         taskNameEditText.setOnClickListener(v -> taskNameEditText.setCursorVisible(true));
         taskNameEditText.setOnFocusChangeListener((v, hasFocus) -> taskNameEditText.setCursorVisible(hasFocus));
@@ -131,6 +117,31 @@ public class DetailsFragment extends Fragment {
                 handled = true;
             }
             return handled;
+        });
+
+        // Set click listener on due date field to open the DatePickerDialog
+        CardView dueDatePicker = view.findViewById(R.id.datePickerCard);
+        dueDatePicker.setOnClickListener(v -> createDatePicker());
+
+        // Set a listener on the "x" button next to the due date to remove the task's due date when pressed
+        removeDueDateButton.setOnClickListener(v -> {
+            viewModel.selectedTask.setDueDate(null);
+            dueDateTextView.setText(R.string.due_date_label_default);
+            removeDueDateButton.setVisibility(View.GONE);
+        });
+
+        // Set a listener on the notes field to hide the character counter when the field is empty
+        taskNotesEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                taskNotesLayout.setCounterEnabled(editable.toString().length() != 0);
+            }
         });
 
         // Set listener on the save button to push updates to the db and go back to the list
@@ -151,7 +162,7 @@ public class DetailsFragment extends Fragment {
         return view;
     }
 
-    public void createDatePicker() {
+    void createDatePicker() {
         final Calendar calendar = Calendar.getInstance();
         if (viewModel.selectedTask.getDueDate() != null) {
             calendar.setTime(viewModel.selectedTask.getDueDate());
@@ -170,5 +181,31 @@ public class DetailsFragment extends Fragment {
         }, currentYear, currentMonth, currentDay);
 
         datePickerDialog.show();
+    }
+
+    /**
+     * Function to check if there are unsaved changes to the task.
+     * Currently only used in the OnBackPressedCallback that is set up in DetailFragment's OnCreate(). This check has
+     * been isolated to a separate function to maintain readability.
+     * @return boolean value indicating the presence of unsaved changes.
+     */
+    boolean hasUnsavedChanges() {
+
+        // If any of the name, status, or notes values differ between the VM and ref. task, there are unsaved changes
+        if (!(taskNameEditText.getText().toString().equals(referenceTask.getName())
+                && taskStatusCheckBox.isChecked() == referenceTask.getComplete()
+                && taskNotesEditText.getText().toString().equals(referenceTask.getNotes()))) {
+            return true;
+        }
+
+        if (viewModel.selectedTask.getDueDate() == null ^ referenceTask.getDueDate() == null) {
+            // If the viewModel XOR reference task due dates are null (not both), then we have unsaved changes
+            return true;
+        } else if (viewModel.selectedTask.getDueDate() != null && viewModel.selectedTask.getDueDate() != null) {
+            // If neither due date is null, check that the two are equal and return the inverse (equality = no changes)
+            return !(viewModel.selectedTask.getDueDate().equals(referenceTask.getDueDate()));
+        }
+
+        return false;
     }
 }
