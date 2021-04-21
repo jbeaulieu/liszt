a74302a3ee3b784e8c3a1ce184b9502d7000b9ff
oguevara <oscar.guevara55@gmail.com>
2021-04-20 20:28:23 -0500

    Added drag and drop functionality to Tasks. Order persistence not enabled yet.

--- app/src/main/java/com/jbproductions/liszt/ListFragment.java
+++ app/src/main/java/com/jbproductions/liszt/ListFragment.java
@@ -2,7 +2,6 @@ package com.jbproductions.liszt;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -24,9 +23,11 @@ import androidx.recyclerview.selection.ItemKeyProvider;
 import androidx.recyclerview.selection.Selection;
 import androidx.recyclerview.selection.SelectionTracker;
 import androidx.recyclerview.selection.StorageStrategy;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import java.util.Collections;
 
 /**
  * Host fragment for viewing a list. This fragment provides the main view for the application.
@@ -34,7 +35,28 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class ListFragment extends Fragment {
 
     SelectionTracker<Long> mSelectionTracker;
+    // TODO rename away from click and to check/checked/complete/status
     TaskClickInterface mTaskClickInterface;
+    ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP | ItemTouchHelper.DOWN | ItemTouchHelper.START | ItemTouchHelper.END, 0) {
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder,
+                              @NonNull RecyclerView.ViewHolder target) {
+            int fromPosition = viewHolder.getAdapterPosition();
+            int toPosition = target.getAdapterPosition();
+
+            Collections.swap(mViewModel.getAllTasks().getValue(), fromPosition, toPosition);
+
+            recyclerView.getAdapter().notifyItemMoved(fromPosition, toPosition);
+
+            return false;
+        }
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+        }
+    };
     private EditText newTaskText;
     private ViewModel mViewModel;
     private boolean singleItemSelected;
@@ -77,7 +99,8 @@ public class ListFragment extends Fragment {
 
                 Selection<Long> selectedItems = mSelectionTracker.getSelection();
 
-                if (selectedItems.size() == 1) {
+                // TODO should this check use the singleItemSelected variable?
+                if (singleItemSelected) {
                     for (Long selectedItem : selectedItems) {
                         mViewModel.selectedTask = mViewModel.getTaskById(selectedItem);
                     }
@@ -131,7 +154,7 @@ public class ListFragment extends Fragment {
 
         mTaskClickInterface = task -> {
             mViewModel.update(task);
-            Log.d("HERE", "HERE");
+            // Log.d("HERE", "HERE");
         };
 
         RecyclerView listRecyclerView = view.findViewById(R.id.list_recycler_view);
@@ -204,6 +227,8 @@ public class ListFragment extends Fragment {
                 requireActivity().invalidateOptionsMenu();
             }
         });
+         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+         itemTouchHelper.attachToRecyclerView(listRecyclerView);
     }
 
     interface TextEditInterface {
@@ -279,4 +304,5 @@ public class ListFragment extends Fragment {
             return null;
         }
     }
+
 }
