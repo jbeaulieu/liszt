a15f71f21f4abdb0329c20189012ea852b57c872
oalejandro <oguevara@alum.mit.edu>
2022-06-29 04:57:01 -0700

    Added temporary shell implementation for settings menu. Minor formatting of main activity.

--- app/src/main/java/com/jbproductions/liszt/MainActivity.kt
+++ app/src/main/java/com/jbproductions/liszt/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jbproductions.liszt
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -11,6 +12,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.jbproductions.liszt.databinding.ActivityMainBinding
+import com.jbproductions.liszt.models.ListModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,7 +27,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navHostFragment =
+        val navHostFragment: NavHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
         val navController: NavController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(navController.graph)
@@ -33,23 +35,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                val toast: Toast = Toast.makeText(
+                    applicationContext,
+                    "Settings page not implemented yet.",
+                    Toast.LENGTH_SHORT
+                )
+                toast.show()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        val navController: NavController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }
